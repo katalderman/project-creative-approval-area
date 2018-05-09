c010c4c7155e643ad2eb18e9a6e91530e92e3c4e
KatAlderman <kat@uxkat.co>
2018-05-09 10:45:17 -0400

    abides by convention with environment variables

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const multer = require('multer'); // file storing middleware
 mongoose.Promise = Promise;
 mongoose
   // .connect('mongodb://localhost/project-creative-app', {useMongoClient: true})
-.connect(process.env.MONGOURI, {useMongoClient: true})
+.connect(process.env.MONGODB_URI, {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
