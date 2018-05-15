f0f929afcbbd98155d7566432c08206c8bbc20cd
KatAlderman <kat@uxkat.co>
2018-05-14 21:01:31 -0400

    fixes delete project functionality and dropdowns for client/owner in project edit screen

--- routes/index.js
+++ routes/index.js
@@ -25,81 +25,39 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-// // PROJECT CREATE GET
-// router.get("/projectcreate", ensureLogin.ensureLoggedIn(), (req, res) => {
-//   // console.log("the user is: ", req.user)
-//   if(req.user.role === 'ADMIN'){
-
-//     User.find({})
-//   .then( user => {
-
-//     res.render("projectcreate", { user });
-//   }) } else {
-//     res.send("Sorry!")
-//   }
-// });
-
-
-// // PROJECT CREATE POST
-// router.post('/projectcreate',uploadCloud.single('photo'),(req, res, next) => {
-//     const newProject = new Project ({
-//       owner: req.body.theOwner,
-//       client: req.body.theClient,
-//       projectname: req.body.theProjectname,
-//       projectmessage: req.body.theProjectmessage
-//       // need to store component (project images) but not sure how
-//     });
-//     newProject.save((err) => {
-//       if (err) {
-//         res.render("projectcreate", { message: "Something went wrong" });
-//       } else {
-//         // req.session.currentUser = user;
-//         res.redirect("/dashboardAdmin");
-//     }
-//     });
-//   });
-
-
-function renderDashboard(req,res,next) {
-  User.find({})
-  .then( user => {
-    console.log(user);
-      res.render('dashboardAdmin', { 
-        user: user,
-        redirectLocation: req.protocol+'://'+req.hostname+':3000'+'/dashboardAdmin'
-       });
-  } )
+/* render dashboard contents -- project */
+function renderDashboard(req,res,next) { 
+  Project.find({})
+  .then( project => {
+    // console.log(project);
+    User.find({})
+    .then( user => {
+      // console.log(user);
+        res.render('dashboardAdmin', { 
+          user: user,
+          project: project,
+          redirectLocation: req.protocol+'://'+req.hostname+':3000'+'/dashboardAdmin'
+         });
+    } )
+    .catch( error => {
+        console.log("Error while displaying:", error );
+    } )
   .catch( error => {
       console.log("Error while displaying:", error );
-  } )}
-
+  });
+});
+}
 
  // serve up the admin area
  router.get('/dashboardAdmin', function(req, res, next){
    renderDashboard(req,res,next);
-  // // console.log(req.session);
-  // User.find({})
-  // .then( user => {
-  //   console.log(user);
-  //     res.render('dashboardAdmin', { user });
-  // } )
-  // .catch( error => {
-  //     console.log("Error while displaying:", error );
-  // } )
-  // //  res.render('dashboardAdmin');
  });
-
-//  //Route 4: serve up the project details & approval/denial options
-//  router.get('/projectdetails', function(req, res){
-//    res.render('projectdetails');
-//  });
  
  // serve up the user details
  router.get('/viewuser', function(req, res){
    console.log('who is logged in: ', req.user)
    res.render('passport/viewuser', { user: req.user });
  });
-
 
 // PROJECT CREATE GET
 router.get("/projectcreate", ensureLogin.ensureLoggedIn(), (req, res) => {
@@ -129,67 +87,58 @@ router.post("/projectcreate", uploadCloud.single('imgPath'), function(req,res) {
 })
 });
 
+// PROJECT EDIT - FORM DISPLAY
+router.get('/projectdetails/:id', (req, res, next) => {
+  User.find() 
+  .then((users) => {
+    const projectId = req.params.id;
+    Project.findById(projectId)
+    .then(projectFromDb => {
+      res.render('projectdetails', {project: projectFromDb, user:users})
+    })
 
- //Route 4: serve up the project details & approval/denial options
- router.get('/projectdetails', function(req, res){
-  res.render('projectdetails');
-});
+  })
 
-console.log(router.stack)
+}) 
 
-
-
-
-    // const projOwner= req.body.theOwner;
-    // const projClient= req.body.theClient;
-    // const projProjectname= req.body.theProjectname;
-    // const projProjectmessage= req.body.theProjectmessage;
-
-
-// // PROJECT CREATE POST
-// router.post('/create',uploadCloud.single('photo'),(req, res, next) => {
-//   Project.create({
-//     owner: req.body.owner,
-//     client: req.body.client,
-//     projectname: req.body.projectname,
-//     projectmessage: req.body.projectmessage,
-//     // component: req.body.component, //im not sure what to do about the object in the db here
-//     // imgPath: req.file.url, // and here
-//     password: req.body.password
-//   })
-//   .then((theProject)=>{
-//     res.redirect('/dashboardAdmin')
-//   })
-// });
-
-
-
-// // DISPLAY ALL THE USERS
-// // url: localhost:3000/viewuser
-// router.get('/viewuser', (req, res, next) => {
-//   console.log(req.user);
-//   User.findById(req.user._id)
-//   .then( responseFromDB => {
-//     console.log(responseFromDB);
-//       res.render('passport/viewuser', { user: responseFromDB });
-//   } )
-//   .catch( error => {
-//       console.log("Error while displaying:", error );
-//   } )
-// });
+// PROJECT EDIT - POST ROUTE
+router.post('/projectdetails/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  const editedowner = req.body.editedowner;
+  const editedclient = req.body.editedclient;
+  const editedprojectname = req.body.editedprojectname;
+  const editedprojectmessage = req.body.editedprojectmessage;
+  // console.log("edit page: ", req.body, projectId)
+  Project.findByIdAndUpdate(projectId, {
+    owner: editedowner,
+    client: editedclient,
+    projectName: editedprojectname,
+    projectMessage: editedprojectmessage
+})
+  .then(() => {
+      res.redirect('/dashboardAdmin')
+  })
+  .catch( error => {
+      console.log("Error while updating: ", error)
+  })
+})
 
 
+// PROJECT DELETE 
+router.delete('/projects/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  Project.findByIdAndRemove(projectId)
+  .then(() => {
+    // console.log("--------------------deleted")
+    res.status(200).send();
+    // req.method = 'GET'
+    //   res.redirect("/dashboardAdmin");
+  })
+  .catch( error => {
+      console.log("Error while deleting: ", error)
+  })
+})
 
-// /* GET dashboard for admins */
-// router.get('/dashboardAdmin', (req, res, next) => {
-//   Project.find()
-//   .then((allProjects)=> {
-//     res.render('index',{projects:allProjects})
-//   })
-//   .catch((err)=>{
-//     console.log(err)
-//   })
-// });
 
-
+// console.log(router.stack)
 module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -1,128 +1,90 @@
-const express     = require('express');
-const router      = express.Router();
-const multer      = require('multer');
-const cloudinary  = require('cloudinary');
-const storage     = require('multer-storage-cloudinary');
-const uploadCloud = require("../config/cloudinary")
-const bodyParser   = require('body-parser'); //cleans our req.body
-const cookieParser = require('cookie-parser');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const Project      = require("../models/project");
-const User         = require("../models/user");
-const session      = require("express-session");
-const bcrypt       = require("bcrypt");
-const passport     = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
-const flash        = require("connect-flash");
-const ensureLogin  = require("connect-ensure-login");
+const express        = require("express");
+const router         = express.Router();
+const ensureLogin   = require("connect-ensure-login");
+const passport      = require("passport");
 
+// User model
+const Project           = require("../models/project");
+// const userRouter  = express.Router();
 
-// PROJECT CREATE GET
-router.get("/projectcreate", ensureLogin.ensureLoggedIn(), (req, res) => {
-  // console.log("the user is: ", req.user)
-  if(req.user.role === 'ADMIN'){
+// projectcreate GET
+router.get("/projectcreate", (req, res, next) => {
+  res.render("projectcreate");
+});
 
-    User.find({})
-  .then( user => {
-
-    res.render("projectcreate", { user });
-  }) } else {
-    res.send("Sorry!")
+// projectcreate POST
+router.post("/projectcreate", (req, res, next) => {
+  const projectid = req.params.id;
+  const editedowner = req.body.editedowner;
+  const editedclient = req.body.editedclient;
+  const editedprojectname = req.body.editedprojectname;
+  const editedprojectmessage = req.body.editedprojectmessage;
   }
-});
 
-// PROJECT CREATE POST
-router.post("/projectcreate", uploadCloud.single('imgPath'), function(req,res) {
-    Project.create({
-      owner: req.body.theOwner,
-      client: req.body.theClient,
-      projectName: req.body.theProjectName,
-      projectMessage: req.body.theProjectMessage,
-      imgPath: req.file.url
-    })
-    .then ((theUser) => {
-  res.redirect('/')
+  Project.findOne({ email }, "email", (err, project) => {
+    if (project !== null) {
+      res.render("projectcreate", { message: "The project already exists" });
+      return;
+    }
+
+    const newProject = new Project({
+      owner,
+      client,
+      projectname,
+      projectmessage,
+      isapproved
+    });
+
+    newProject.save((err) => {
+      if (err) {
+        res.render("projectcreate", { message: "Something went wrong" });
+      } else {
+        res.redirect("/dashboardAdmin");
+      }
+    });
+
+// PROJECT EDIT - FORM DISPLAY
+router.get('/projectdetails/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  Project.findById(projectId)
+  .then(projectFromDb => {
+    res.render('projectdetails', {project: projectFromDb})
+  })
+}) 
+
+// PROJECT EDIT - POST ROUTE
+router.post('/projectdetails/:id', (req, res, next) => {
+  const projectid = req.params.id;
+  const editedowner = req.body.editedowner;
+  const editedclient = req.body.editedclient;
+  const editedprojectname = req.body.editedprojectname;
+  const editedprojectmessage = req.body.editedprojectmessage;
+  // console.log("edit page: ", req.body, projectId)
+  Project.findByIdAndUpdate(projectId, {
+    owner: editedowner,
+    client: editedclient,
+    projectName: editedprojectname,
+    projectMessage: editedprojectmessage
 })
-});
+  .then(() => {
+      res.redirect('/projectdetails')
+  })
+  .catch( error => {
+      console.log("Error while updating: ", error)
+  })
+})
 
 
-// // projectcreate POST
-// router.post("/projectcreate", (req, res, next) => {
-// const owner = req.body.theOwner;
-// const client = req.body.theClient;
-// const projectName = req.body.theProjectName;
-// const projectMessage = req.body.theProjectMessage;
-// const imgPath = req.file.url;
+// PROJECT DELETE 
+router.delete('projects/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  User.findByIdAndRemove(projectId)
+  .then(() => {
+      res.redirect("/dashboardAdmin");
+  })
+  .catch( error => {
+      console.log("Error while deleting: ", error)
+  })
+})
 
-//     const newProject = new Project({
-//       owner,
-//       client,
-//       projectName,
-//       projectMessage,
-//       imgPath
-//     });
-
-//     newProject.save((err) => {
-//       if (err) {
-//         res.render("passport/projectcreate", { message: "Something went wrong" });
-//       } else {
-//         // req.session.currentProject = project;
-//         res.redirect("/dashboardAdmin");
-//       }
-//     });
-//   });
-
-
-
- //Route 4: serve up the project details & approval/denial options
- router.get('/projectdetails', function(req, res){
-   res.render('projectdetails');
- });
- 
-console.log(router.stack)
 module.exports = router;
-
-
-
-
-
-// // PROJECT CREATE POST
-// projectRouter.post("/projectcreate",uploadCloud.single('photo'),(req, res, next) => {
-//   const newProject = new Project ({
-//     owner: req.body.theOwner,
-//     client: req.body.theClient,
-//     projectname: req.body.theProjectname,
-//     projectmessage: req.body.theProjectmessage
-//     // need to store component (project images) but not sure how
-//   });
-//   newProject.save((err) => {
-//     if (err) {
-//       res.render("projectcreate", { message: "Something went wrong" });
-//     } else {
-//       // req.session.currentUser = user;
-//       res.redirect("/dashboardAdmin");
-//   }
-//   });
-// });
-
-
-  //   const newProject = new Project ({
-  //     owner: req.body.theOwner,
-  //     client: req.body.theClient,
-  //     projectname: req.body.theProjectname,
-  //     projectmessage: req.body.theProjectmessage
-  //     // need to store component (project images) but not sure how
-  //   });
-  //   newProject.save((err) => {
-  //     if (err) {
-  //       res.render("projectcreate", { message: "Something went wrong" });
-  //     } else {
-  //       // req.session.currentUser = user;
-  //       res.redirect("/dashboardAdmin");
-  //   }
-  //   });
-  // });
